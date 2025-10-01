7a8f7a64e179e096ede59e0dac610880b967ab84
kiptanuiBoaz <boaserem022@gmail.com>
2025-10-01 05:20:06 +0300

    feat: add admin settings

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    read_only_fields = ("slug",)
+    list_filter = ("rating", "author")
+
+
 # Register your models here.
+admin.site.register(Book, BookAdmin)
